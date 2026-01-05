d0d7a8a1e7f5bf1bf12984fd58e19bb605d7de7d
sobermaster9000 <99313967+sobermaster9000@users.noreply.github.com>
2026-01-05 20:30:13 +0800

    add chunk meshing

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #define HEIGHT 600
 #define BUFFER_SIZE 1024
 
+#define CHUNK_SIZE 16
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
@@ -14,34 +16,120 @@ void processInput(GLFWwindow* window) {
     glfwSetWindowShouldClose(window, true);
 }
 
+float getPerlinHeight(int x, int z, siv::PerlinNoise& perlinNoise) {
+    return (float)(int)(perlinNoise.octave2D_01((float)x * 0.03f, (float)z * 0.03f, 5) * 9.5f);
+}
+
+void generateChunkMeshData(int chunkStartX, int chunkStartZ, siv::PerlinNoise& perlinNoise, std::vector<Vertex>& vertices, std::vector<GLuint>& indices) {
+    int verticesLength;
+    for (int x = chunkStartX; x < chunkStartX + CHUNK_SIZE; x++) {
+        for (int z = chunkStartZ; z < chunkStartZ + CHUNK_SIZE; z++) {
+            float y = getPerlinHeight(x, z, perlinNoise);
+            if (getPerlinHeight(x, z-1, perlinNoise) < y) {
+                // add front face
+                vertices.push_back(Vertex{glm::vec3(x + -0.5f,  y + 0.0f,   z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.0f,  0.5f)});
+                vertices.push_back(Vertex{glm::vec3(x + 0.5f,   y + 0.0f,   z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.25f,  0.5f)});
+                vertices.push_back(Vertex{glm::vec3(x + 0.5f,   y + 1.0f,   z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.25f,  1.0f)});
+                vertices.push_back(Vertex{glm::vec3(x + -0.5f,  y + 1.0f,   z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.0f,  1.0f)});
+                verticesLength = vertices.size();
+                indices.push_back(verticesLength-4);
+                indices.push_back(verticesLength-3);
+                indices.push_back(verticesLength-2);
+                indices.push_back(verticesLength-4);
+                indices.push_back(verticesLength-2);
+                indices.push_back(verticesLength-1);
+            }
+            if (getPerlinHeight(x, z+1, perlinNoise) < y) {
+                // add back face
+                vertices.push_back(Vertex{glm::vec3(x + 0.5f,   y + 0.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.5f,  0.5f)});
+                vertices.push_back(Vertex{glm::vec3(x + -0.5f,  y + 0.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.75f,  0.5f)});
+                vertices.push_back(Vertex{glm::vec3(x + -0.5f,  y + 1.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.75f,  1.0f)});
+                vertices.push_back(Vertex{glm::vec3(x + 0.5f,   y + 1.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.5f,  1.0f)});
+                verticesLength = vertices.size();
+                indices.push_back(verticesLength-4);
+                indices.push_back(verticesLength-3);
+                indices.push_back(verticesLength-2);
+                indices.push_back(verticesLength-4);
+                indices.push_back(verticesLength-2);
+                indices.push_back(verticesLength-1);
+            }
+            if (getPerlinHeight(x+1, z, perlinNoise) < y) {
+                // add right face
+                vertices.push_back(Vertex{glm::vec3(x + 0.5f,   y + 0.0f,   z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.25f,  0.5f)});
+                vertices.push_back(Vertex{glm::vec3(x + 0.5f,   y + 0.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.5f,  0.5f)});
+                vertices.push_back(Vertex{glm::vec3(x + 0.5f,   y + 1.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.5f,  1.0f)});
+                vertices.push_back(Vertex{glm::vec3(x + 0.5f,   y + 1.0f,   z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.25f,  1.0f)});
+                verticesLength = vertices.size();
+                indices.push_back(verticesLength-4);
+                indices.push_back(verticesLength-3);
+                indices.push_back(verticesLength-2);
+                indices.push_back(verticesLength-4);
+                indices.push_back(verticesLength-2);
+                indices.push_back(verticesLength-1);
+            }
+            if (getPerlinHeight(x-1, z, perlinNoise) < y) {
+                // add left face
+                vertices.push_back(Vertex{glm::vec3(x + -0.5f,  y + 0.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.75f,  0.5f)});
+                vertices.push_back(Vertex{glm::vec3(x + -0.5f,  y + 0.0f,   z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(1.0f,  0.5f)});
+                vertices.push_back(Vertex{glm::vec3(x + -0.5f,  y + 1.0f,   z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(1.0f,  1.0f)});
+                vertices.push_back(Vertex{glm::vec3(x + -0.5f,  y + 1.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.75f,  1.0f)});
+                verticesLength = vertices.size();
+                indices.push_back(verticesLength-4);
+                indices.push_back(verticesLength-3);
+                indices.push_back(verticesLength-2);
+                indices.push_back(verticesLength-4);
+                indices.push_back(verticesLength-2);
+                indices.push_back(verticesLength-1);
+            }
+            // add top face
+            vertices.push_back(Vertex{glm::vec3((float)x + -0.5f,  (float)y + 1.0f,   (float)z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.0f,  0.0f)});
+            vertices.push_back(Vertex{glm::vec3((float)x + 0.5f,   (float)y + 1.0f,   (float)z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.25f,  0.0f)});
+            vertices.push_back(Vertex{glm::vec3((float)x + 0.5f,   (float)y + 1.0f,   (float)z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.25f,  0.5f)});
+            vertices.push_back(Vertex{glm::vec3((float)x + -0.5f,  (float)y + 1.0f,   (float)z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.0f,  0.5f)});
+            verticesLength = vertices.size();
+            indices.push_back(verticesLength-4);
+            indices.push_back(verticesLength-3);
+            indices.push_back(verticesLength-2);
+            indices.push_back(verticesLength-4);
+            indices.push_back(verticesLength-2);
+            indices.push_back(verticesLength-1);
+        }
+    }
+}
+
 void makeBlockVertices(std::vector<Vertex>& vertices, float x, float y, float z) {
                         // vertices                                              colors                                 texCoords //
-
+    // front face
     vertices[0] = Vertex{glm::vec3(x + -0.5f,  y + 0.0f,   z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.0f,  0.5f)};
     vertices[1] = Vertex{glm::vec3(x + 0.5f,   y + 0.0f,   z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.25f,  0.5f)};
     vertices[2] = Vertex{glm::vec3(x + 0.5f,   y + 1.0f,   z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.25f,  1.0f)};
     vertices[3] = Vertex{glm::vec3(x + -0.5f,  y + 1.0f,   z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.0f,  1.0f)};
 
+    // right face
     vertices[4] = Vertex{glm::vec3(x + 0.5f,   y + 0.0f,   z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.25f,  0.5f)};
     vertices[5] = Vertex{glm::vec3(x + 0.5f,   y + 0.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.5f,  0.5f)};
     vertices[6] = Vertex{glm::vec3(x + 0.5f,   y + 1.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.5f,  1.0f)};
     vertices[7] = Vertex{glm::vec3(x + 0.5f,   y + 1.0f,   z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.25f,  1.0f)};
 
+    // back face
     vertices[8] = Vertex{glm::vec3(x + 0.5f,   y + 0.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.5f,  0.5f)};
     vertices[9] = Vertex{glm::vec3(x + -0.5f,  y + 0.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.75f,  0.5f)};
     vertices[10] = Vertex{glm::vec3(x + -0.5f,  y + 1.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.75f,  1.0f)};
     vertices[11] = Vertex{glm::vec3(x + 0.5f,   y + 1.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.5f,  1.0f)};
 
+    // left face
     vertices[12] = Vertex{glm::vec3(x + -0.5f,  y + 0.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.75f,  0.5f)};
     vertices[13] = Vertex{glm::vec3(x + -0.5f,  y + 0.0f,   z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(1.0f,  0.5f)};
     vertices[14] = Vertex{glm::vec3(x + -0.5f,  y + 1.0f,   z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(1.0f,  1.0f)};
     vertices[15] = Vertex{glm::vec3(x + -0.5f,  y + 1.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.75f,  1.0f)};
 
+    // top face
     vertices[16] = Vertex{glm::vec3(x + -0.5f,  y + 1.0f,   z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.0f,  0.0f)};
     vertices[17] = Vertex{glm::vec3(x + 0.5f,   y + 1.0f,   z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.25f,  0.0f)};
     vertices[18] = Vertex{glm::vec3(x + 0.5f,   y + 1.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.25f,  0.5f)};
     vertices[19] = Vertex{glm::vec3(x + -0.5f,  y + 1.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.0f,  0.5f)};
 
+    // bottom face
     vertices[20] = Vertex{glm::vec3(x + -0.5f,  y + 0.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.25f,  0.0f)};
     vertices[21] = Vertex{glm::vec3(x + 0.5f,   y + 0.0f,   z + 0.5f),       glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.5f,  0.0f)};
     vertices[22] = Vertex{glm::vec3(x + 0.5f,   y + 0.0f,   z + -0.5f),      glm::vec3(1.0f,   1.0f,   1.0f),       glm::vec2(0.5f,  0.5f)};
@@ -76,25 +164,25 @@ int main() {
     // initialize shaders
     Shader shader("shaders/vertex.glsl", "shaders/fragment.glsl");
 
-    std::vector<GLuint> indices{
-        0, 1, 2,
-        0, 2, 3,
+    // std::vector<GLuint> indices{
+    //     0, 1, 2,
+    //     0, 2, 3,
 
-        4, 5, 6,
-        4, 6, 7,
+    //     4, 5, 6,
+    //     4, 6, 7,
 
-        8, 9, 10,
-        8, 10, 11,
+    //     8, 9, 10,
+    //     8, 10, 11,
 
-        12, 13, 14,
-        12, 14, 15,
+    //     12, 13, 14,
+    //     12, 14, 15,
 
-        16, 17, 18,
-        16, 18, 19,
+    //     16, 17, 18,
+    //     16, 18, 19,
 
-        20, 21, 22,
-        20, 22, 23
-    };
+    //     20, 21, 22,
+    //     20, 22, 23
+    // };
 
     std::vector<Texture> textures{
         Texture("textures/grass.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE)
@@ -102,16 +190,35 @@ int main() {
 
     siv::PerlinNoise perlinNoise{siv::PerlinNoise::seed_type{69420u}};
     std::vector<Mesh> meshes;
-    std::vector<Vertex> vertices(24);
-    for (int i = 0; i < 100; i++) {
-        for (int j = 0; j < 100; j++) {
-            makeBlockVertices(vertices,
-                (float)i,
-                (float)(int)(perlinNoise.octave2D_01((float)i * 0.03f, (float)j * 0.03f, 5) * 9.5f),
-                (float)j);
-            meshes.push_back(Mesh(vertices, indices, textures));
-        }
-    }
+    std::vector<Vertex> vertices;
+    std::vector<GLuint> indices;
+    generateChunkMeshData(0, 0, perlinNoise, vertices, indices);
+    meshes.push_back(Mesh(vertices, indices, textures));
+    vertices.clear();
+    indices.clear();
+    generateChunkMeshData(0, CHUNK_SIZE, perlinNoise, vertices, indices);
+    meshes.push_back(Mesh(vertices, indices, textures));
+    vertices.clear();
+    indices.clear();
+    generateChunkMeshData(CHUNK_SIZE, 0, perlinNoise, vertices, indices);
+    meshes.push_back(Mesh(vertices, indices, textures));
+    vertices.clear();
+    indices.clear();
+    generateChunkMeshData(CHUNK_SIZE, CHUNK_SIZE, perlinNoise, vertices, indices);
+    meshes.push_back(Mesh(vertices, indices, textures));
+
+    // siv::PerlinNoise perlinNoise{siv::PerlinNoise::seed_type{69420u}};
+    // std::vector<Mesh> meshes;
+    // std::vector<Vertex> vertices(24);
+    // for (int i = 0; i < 100; i++) {
+    //     for (int j = 0; j < 100; j++) {
+    //         makeBlockVertices(vertices,
+    //             (float)i,
+    //             (float)(int)(perlinNoise.octave2D_01((float)i * 0.03f, (float)j * 0.03f, 5) * 9.5f),
+    //             (float)j);
+    //         meshes.push_back(Mesh(vertices, indices, textures));
+    //     }
+    // }
 
     // enable depth testing for textures
     glEnable(GL_DEPTH_TEST);
@@ -157,7 +264,6 @@ int main() {
         camera.updateMatrix(90.0f, 0.1f, 100.0f, 1.0f);
         
         // draw meshes
-        // block.draw(shader, camera);
         for (Mesh& mesh: meshes)
             mesh.draw(shader, camera);
 
